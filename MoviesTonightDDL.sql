1bbc769ebaafde179d490fdc8d4b14173526e96a
anthonyiorio <anthony.iorio@student.fairfield.edu>
2017-11-18 14:33:11 -0500

    Fixed the ERD, created the database and tables.
    Lot of good things in the works. Corrected the ERD to account for the realities of naming things in SQL.

---
+++ MoviesTonightDDL.sql
@@ -0,0 +1,38 @@
+CREATE DATABASE MoviesTonight;
+
+CREATE TABLE Artist (
+	ID int(11) NOT NULL auto_increment,
+    TName varchar(50) default NULL,
+	PRIMARY KEY (ID)
+);
+
+CREATE TABLE Credit (
+	ID int(11) NOT NULL auto_increment,
+    CCode varchar(1) default NULL,
+    ArtistID int(11) default NULL,
+    MovieID int(11) default NULL,
+	PRIMARY KEY (ID)
+);
+
+CREATE TABLE Movie (
+	ID int(11) NOT NULL auto_increment,
+    Title varchar(50) default NULL,
+    Rating varchar(5) default NULL,
+	PRIMARY KEY (ID)
+);
+
+CREATE TABLE ShowTime (
+	ID int(11) NOT NULL auto_increment,
+    ShowTime time default NULL,
+    MovieID int(11) default NULL,
+	TheaterID int(11) default NULL,
+    PRIMARY KEY (ID)
+);
+
+CREATE TABLE Theater (
+	ID int(11) NOT NULL auto_increment,
+    TName varchar(50) default NULL,
+    Location varchar(50) default NULL,
+    Phone int(10) default NULL,
+    PRIMARY KEY (ID)
+);
